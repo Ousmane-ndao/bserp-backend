9c005f0298799c022cc1f33cba25a147e3554ad0
Ousmane ndao <o.ndao5@isepat.edu.sn>
2026-07-16 19:28:04 +0000

    modification16

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
+
+class Authenticate extends Middleware
+{
+    protected function redirectTo(Request $request)
+    {
+        if ($request->expectsJson() || $request->is('api/*')) {
+            abort(response()->json(['message' => 'Non authentifié.'], 401));
+        }
+        return route('login');
+    }
+}
