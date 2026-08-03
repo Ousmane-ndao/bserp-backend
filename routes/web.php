f8f1ca8afa28ccfcbcb19ea1a08035d65b1bc0c9
Ousmane ndao <o.ndao5@isepat.edu.sn>
2026-08-03 11:35:40 +0000

    Ajout de la route de débogage /debug-logs

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route temporaire pour lire les logs de Laravel
+Route::get('/debug-logs', function () {
+    $logFile = storage_path('logs/laravel.log');
+    if (file_exists($logFile)) {
+        // Affiche le contenu du fichier log directement dans le navigateur
+        return response()->file($logFile, [
+            'Content-Type' => 'text/plain',
+        ]);
+    }
+    return 'Aucun fichier de log trouvé.';
+});
